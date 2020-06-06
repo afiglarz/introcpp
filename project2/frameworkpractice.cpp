e1855beb253efe0f0c73910ab75ab46c49479d68
Andrew <andrew.figlarz@nist.gov>
2020-06-06 10:53:35 -0400

    Making changes Greg proposed on 6-5-20

--- project2/frameworkpractice.cpp
+++ project2/frameworkpractice.cpp
@@ -1,9 +1,5 @@
 // Practice project getting used to the biomeval framework
 
-// Current Issues:
-// - If you give it a directory, make sure you give the ending "/" character,
-// - the current implementation checks for that to recognize a directory.
-
 #include <dirent.h>
 #include <sys/types.h>
 
@@ -12,6 +8,8 @@
 #include <cstdlib>
 #include <cstring>
 
+// Include wherever you use~
+#include <be_io_utility.h>
 #include "frameworkpractice.h"
 
 namespace BE = BiometricEvaluation;
@@ -23,55 +21,56 @@ generateInfo(
 	std::string filename) 
 {
 	namespace BE = BiometricEvaluation;
-	
 	std::string output{};
 
-	auto img = BE::Image::Image::openImage(filename).get();
+	std::shared_ptr<BE::Image::Image> img{};
+
+	try {
+		img = BE::Image::Image::openImage(filename);
+	} catch (const BE::Error::Exception &e) {
+		throw BE::Error::ObjectDoesNotExist("Could Not Open file: " + filename);
+	}
 	
+	// Gets Identifier/Filename
+	std::string id = img->getIdentifier();
 	// Get Compression Algorithm from image
-	BE::Image::CompressionAlgorithm compalg = 
-	(*img).getCompressionAlgorithm();
-
+	BE::Image::CompressionAlgorithm compalg = img->getCompressionAlgorithm();
+	// Gets Dimensions
+	BE::Image::Size dims = img->getDimensions();
 	// Get Resolution of Image 
-	BE::Image::Resolution res = (*img).getResolution();
-
-	// Get Data of Image -- Not Raw -- Is encoded
-	BE::Memory::uint8Array data = (*img).getData();
-
+	BE::Image::Resolution res = img->getResolution();
+	// Get Data of Image -- Not Raw -- Is encoded -- used to get size rn 
+	BE::Memory::uint8Array data = img->getData();
 	// Get Colordepth
-	uint32_t colordepth = (*img).getColorDepth();
-
+	uint32_t colordepth = img->getColorDepth();
 	// Get bitdepth
-	uint16_t bitdepth = (*img).getBitDepth();
-
+	uint16_t bitdepth = img->getBitDepth();
 	// Determines if it has an alpha channel
-	bool hasalpha = (*img).hasAlphaChannel();
+	bool hasalpha = img->hasAlphaChannel();
+	// Works: No Seg Faults -- Isn't needed rn
+	// BE::Memory::uint8Array rawdata = img->getRawData(false);
 
-	// Gets Identifier/Filename - Sometimes messes up the name of file?
-	std::string id = (*img).getIdentifier();
+	// Works: No Seg Faults -- Isn't needed rn
+	// BE::Memory::uint8Array rawgrayscale = img->getRawGrayscaleData(1);
 
-	// Adds Filename
+	// Prints Filename
 	output = output + "ID/Filename: " + id + "\n";
-
-	// Adds Compression Algorithm
+	// Prints Compression Algorithm
 	output = output + "Compression Algorithm: " +
 	std::to_string(static_cast<std::underlying_type
 	<BE::Image::CompressionAlgorithm>::type>(compalg)) + "\n";
-
+	// Pint out Dimensions of Image
+	output = output + "Dimensions: " + to_string(dims) + "\n";
 	// Print out Resolution of Image
 	output = output + "Resolution of Image: " + to_string(res) + "\n";
-
 	// Print out ColorDepth of Image
 	output = output + "Color Depth of Image: " + std::to_string(colordepth) 
 	+ "\n";
-
 	// Print out BitDepth of Image
 	output = output + "Bit Depth of Image: " + std::to_string(bitdepth) + "\n";
-
 	// Print out Data Size of Image
 	output = output + "Data Size of Image: " + std::to_string(data.size()) 
 	+ "\n";
-
 	// Print out Whether it has an alpha channel
 	output = output + "Alpha Channel: " + std::to_string(hasalpha) + "\n";
 
@@ -87,56 +86,48 @@ int main( int argc, char** argv )
 	
 	// Get Filename
 	std::string filename = argv[1];
-	size_t ifdir = filename.find("/");
 
-	if (ifdir == std::string::npos) {
-		// its a file and not a dir:
-		std::string output = generateInfo(filename);
-		std::cout << output << "\n";	
+	if (!BE::IO::Utility::pathIsDirectory(filename)) {
+		// In this case, it is a file and not a directory
+		try {
+			std::string output = generateInfo(filename);
+			std::cout << output << "\n";
+		} catch (BE::Error::ObjectDoesNotExist &e) {
+			std::cerr << e.what() << "\n" << "\n";
+			return EXIT_FAILURE;
+		}	
+
 	} else {
-		// Its a Directory
+		// Its a directory
+		
 		DIR *dr;
 		struct dirent *en;
 		dr = opendir(filename.c_str());
 
 		if (dr) {
 			while ((en = readdir(dr)) != NULL) {
-				try {
-					std::string path = filename + en->d_name;
-					std::string output = generateInfo(path);
+				if (strcmp(en->d_name, ".") != 0 && 
+					strcmp(en->d_name, "..") != 0) {
+					
+					try {
+						std::string path = filename + '/' + en->d_name;
+						std::string output = generateInfo(path);
+						std::cout << output << "\n";
 
-					std::cout << output << "\n";
-
-				} catch (BE::Error::StrategyError& e) {
-					std::cerr << e.what() << "\n" << "\n";
+					} catch (BE::Error::ObjectDoesNotExist &e) {
+						std::cerr << e.what() << "\n" << "\n";
+					}
 				}
 			}
-			closedir(dr);
+		closedir(dr);
 		}
+			
 	}
 	return 0;
 }
-		
-		// Stuff That doesn't seem to work:
 
-		// Executing this results in a calling a pure virtural function error - 
-		// or segfaults - depending on true or false
-		// BE::Memory::uint8Array rawdata = 
-		// (*img).getRawData(false);
-
-		// Segfaults
-		// BE::Memory::uint8Array rawgrayscale =
-		// (*img).getRawGrayscaleData(1);
-
-		// This says its protected: - but you can just do .getData().size()?
-		// uint64_t datasize = (*img).getDataSize();
-	
-		// For some reason returns a memory address
-		// Get Dimensions of Image
-		// BE::Image::Size dims = 
-		// (*img).getDimensions();
-
-		// Gets Status Callback - Also returns a memory address - how to parse?
+		// Ask Greg for clarification on what this is again.
+		// Gets Status Callback
 		// BE::Image::Image::statusCallback_t status = 
 		// (*img).getStatusCallback();
 
